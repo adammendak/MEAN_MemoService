d1f7c2d58445772c996cb131537fe335fd759537
adammendak <adam.mendak@gmail.com>
2017-12-01 08:24:41 +0100

    working on auth

--- auth/server/server.js
+++ auth/server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const app = express();
 const port = process.env.port || 3000;
@@ -13,6 +14,24 @@ mongoose.connect('mongodb://localhost:27017/auth');
 
 const {User} = require('/model/user');
 app.use(bodyParser.json());
+
+
+//POST
+app.post('api/user', (req,res)=> {
+    const user = new User({
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    user.save((err,doc) => {
+        if(err) {
+            res.status(400).send(err);
+        }
+
+        res.send(200).send(doc);
+    })
+
+})
 
 
 
