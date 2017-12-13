0935501119adc6b0be49a6a016a82ed4e7556f51
adammendak <adam.mendak@gmail.com>
2017-12-13 21:34:11 +0100

    more

--- 8_express/app.js
+++ 8_express/app.js
@@ -1,4 +1,6 @@
+// import express from 'express';
 const express = require('express');
+const hbs = require('express-handlebars');
 
 const app = express();
 
@@ -7,8 +9,18 @@ const port = process.env.PORT || 3000;
 //creating static folder
 app.use(express.static(__dirname + "/public"));
 
+
+
+app.engine(".hbs", hbs({
+    extname:".hbs",
+    defaultLayout: "index",
+    layoutsDir: __dirname + "/public/views/"
+}));
+app.set("view engine", "hbs");
+app.set("views", "./public/views");
+
 app.get('/', (req, res) => {
-    res.sendFile('index.html');
+    res.render('index');
 });
 
 app.get('/books', (req,res) => {
@@ -17,4 +29,4 @@ app.get('/books', (req,res) => {
 
 app.listen(port, (err) => {
     console.log('app listening on port ' + port)
-})
+});;
