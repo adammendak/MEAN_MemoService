54dd87e372289ed48c272fecd9be0861c915f556
adammendak <adam.mendak@gmail.com>
2018-02-24 21:37:11 +0100

    exporting to router

--- MemoService/app.js
+++ MemoService/app.js
@@ -5,8 +5,9 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const flash = require('connect-flash');
 const session = require('express-session');
-const app = express();
 
+
+const app = express();
 
 //DB config url string
 const DB = require('./config/database');
@@ -17,10 +18,6 @@ mongoose.connect(DB.mongoURI)
     .then(() => {
     console.log('Mongodb connected ...');
     }).catch(err => console.log(err));
-
-//Load Memo Model
-require('./models/memo');
-const Memo = mongoose.model('memo');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -60,140 +57,16 @@ app.get('/', (req,res)=> {
     res.render('index', {title: title});
 });
 
-//Get Memos
-app.get('/memos', (req,res) => {
-    Memo.find({})
-        .sort({date: 'desc'})
-        .then( memos => {
-            res.render('memos/index', {memos : memos});
-            });
-});
-
-//Add Memos Form
-app.get('/memos/add', (req,res) => {
-    res.render('memos/add');
-});
-
-//Edit Memos
-app.get('/memos/edit/:id', (req,res) => {
-    let errors = [];
-    Memo.findOne({
-        _id: req.params.id
-    }).then( memo => {
-        console.log(`found memo ${memo.toString()}`);
-        res.render('memos/edit', {memo: memo});
-    }).catch(((err) => {
-        errors.push({
-            text: 'error saving to db' + err
-        });
-        res.render('memos/edit', {
-            errors: errors,
-            memo: memo
-        })
-    }));
-
-});
-
 //About Route
 app.get('/about', (req,res) => {
     res.render('about');
 });
 
-app.post('/memos/add', (req,res) => {
-
-    let errors = [];
-
-    if(!req.body.title) {
-        errors.push({
-            text : 'please fill out title'
-        });
-    }
-
-    if(!req.body.details) {
-        errors.push({
-            text : 'please fill out details'
-        });
-    }
-    console.log(req.body);
-
-    if(errors.length> 0) {
-        res.render('memos/add', {
-            errors: errors,
-            title: req.body.title,
-            details: req.body.details
-        })
-    } else {
-        console.log("successfull posting of data");
-        const newMemo = {
-            title: req.body.title,
-            details: req.body.details
-        };
-
-        new Memo(newMemo)
-            .save()
-            .then( () => {
-                req.flash('success_msg', 'Memo has been saved!');
-                res.redirect('/memos');
-            })
-            .catch((err) => {
-                errors.push({
-                    text: 'error saving to db' + err
-                });
-                res.render('memos/add', {
-                    errors: errors,
-                    title: req.body.title,
-                    details: req.body.details
-                })
-            });
-
-        console.log(newMemo.toString());
-    }
-    // toastr.success('success');
-});
-
-app.put('/memos/:id', (req,res) => {
-    Memo.findOne({_id: req.params.id}).then( memo => {
-        memo.title = req.body.title;
-        memo.details = req.body.details;
-        memo.save()
-            .then(memo => {
-                req.flash('success_msg', 'Memo has been updated');
-                console.log(`updated memo : ${memo.toString()}`);
-                res.redirect('/memos');
-        });
-        }).catch(err => {
-        errors.push({
-            text: 'error saving to db' + err
-        });
-        res.redirect(`memos/edit/${req.params.id}`, {
-            errors: errors,
-            title: req.body.title,
-            details: req.body.details
-        })
-    });
-});
-
-//Delete Memo
-app.delete('/memos/:id', (req,res) => {
-    let errors = [];
-    Memo.findOne({_id: req.params.id}).then(memo => {
-        Memo.remove({
-            _id: req.params.id
-        }).then(() => {
-            req.flash('success_msg', 'Memo has been removed');
-            res.redirect('/memos');
-        });
-    }).catch(err => {
-        errors.push({
-            text: 'error deleting ' + err
-        });
-        res.redirect(`memos/edit/${req.params.id}`, {
-            errors: errors,
-            title: req.body.title,
-            details: req.body.details
-        })
-    });
-});
+//Load routes
+const memosRoutes = require('./routes/memos');
+const userRoutes = require('./routes/user');
+app.use('/memos', memosRoutes);
+app.use('/user', userRoutes);
 
 const port = process.env.port || 3000;
 app.listen(3000, () => {
---
+++ MemoService/routes/memos.js
@@ -0,0 +1,140 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+//Load Memo Model
+require('../models/memo');
+const Memo = mongoose.model('memo');
+
+
+//Get Memos
+router.get('/', (req,res) => {
+    Memo.find({})
+        .sort({date: 'desc'})
+        .then( memos => {
+            res.render('memos/index', {memos : memos});
+        });
+});
+
+//Edit Memos
+router.get('/edit/:id', (req,res) => {
+    let errors = [];
+    Memo.findOne({
+        _id: req.params.id
+    }).then( memo => {
+        console.log(`found memo ${memo.toString()}`);
+        res.render('memos/edit', {memo: memo});
+    }).catch(((err) => {
+        errors.push({
+            text: 'error saving to db' + err
+        });
+        res.render('memos/edit', {
+            errors: errors,
+            memo: memo
+        })
+    }));
+});
+
+
+//Add Memos Form
+router.get('/add', (req,res) => {
+    res.render('memos/add');
+});
+
+
+router.post('/add', (req,res) => {
+
+    let errors = [];
+
+    if(!req.body.title) {
+        errors.push({
+            text : 'please fill out title'
+        });
+    }
+
+    if(!req.body.details) {
+        errors.push({
+            text : 'please fill out details'
+        });
+    }
+    console.log(req.body);
+
+    if(errors.length> 0) {
+        res.render('memos/add', {
+            errors: errors,
+            title: req.body.title,
+            details: req.body.details
+        })
+    } else {
+        console.log("successfull posting of data");
+        const newMemo = {
+            title: req.body.title,
+            details: req.body.details
+        };
+
+        new Memo(newMemo)
+            .save()
+            .then( () => {
+                req.flash('success_msg', 'Memo has been saved!');
+                res.redirect('/memos');
+            })
+            .catch((err) => {
+                errors.push({
+                    text: 'error saving to db' + err
+                });
+                res.render('memos/add', {
+                    errors: errors,
+                    title: req.body.title,
+                    details: req.body.details
+                })
+            });
+
+        console.log(newMemo.toString());
+    }
+});
+
+router.put('/:id', (req,res) => {
+    Memo.findOne({_id: req.params.id}).then( memo => {
+        memo.title = req.body.title;
+        memo.details = req.body.details;
+        memo.save()
+            .then(memo => {
+                req.flash('success_msg', 'Memo has been updated');
+                console.log(`updated memo : ${memo.toString()}`);
+                res.redirect('/memos');
+            });
+    }).catch(err => {
+        errors.push({
+            text: 'error saving to db' + err
+        });
+        res.redirect(`/memos/edit/${req.params.id}`, {
+            errors: errors,
+            title: req.body.title,
+            details: req.body.details
+        })
+    });
+});
+
+//Delete Memo
+router.delete('/:id', (req,res) => {
+    let errors = [];
+    Memo.findOne({_id: req.params.id}).then(memo => {
+        Memo.remove({
+            _id: req.params.id
+        }).then(() => {
+            req.flash('success_msg', 'Memo has been removed');
+            res.redirect('/memos');
+        });
+    }).catch(err => {
+        errors.push({
+            text: 'error deleting ' + err
+        });
+        res.redirect(`/memos/edit/${req.params.id}`, {
+            errors: errors,
+            title: req.body.title,
+            details: req.body.details
+        })
+    });
+});
+
+module.exports = router;
