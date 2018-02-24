5b69d92d25bcb876aa29340bb7d99ad2327ede9c
adammendak <adam.mendak@gmail.com>
2018-02-24 10:35:58 +0100

    layout

--- MemoService/app.js
+++ MemoService/app.js
@@ -1,13 +1,32 @@
 const express = require('express');
+const hbs = require('express-handlebars');
 
 const app = express();
 const port = 3000;
 
+//How middleware works
+// app.use(function (req, res, next) {
+//     console.log(Date.now() + " Now time");
+//     req.name = "Adam";
+//     next();
+// });
+
+//Handlebars middleware
+app.engine('handlebars', hbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 //Index Route
-app.get("/*", (req, res)=> {
-    res.writeHead("Content-Type", "application/json");
-    res.end("works ! ")
-})
+app.get("/", (req,res)=> {
+    // res.send("works");
+    const title = 'Index Page';
+    res.render('index', {title: title});
+});
+
+
+//About Route
+app.get("/about", (req,res) => {
+    res.render('about');
+});
 
 app.listen(3000, () => {
     console.log(`port listening on port ${port}`);
