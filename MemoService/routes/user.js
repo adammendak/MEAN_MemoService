4ca0db535e8a3db3b81f72c3af88cbd461fa5949
adammendak <adam.mendak@gmail.com>
2018-02-26 13:49:43 +0100

    refactored user rotuer

--- MemoService/routes/user.js
+++ MemoService/routes/user.js
@@ -6,17 +6,12 @@ const passport = require('passport');
 //Load User Model
 const User = require('../models/User');
 
-//User login route
+//User Login Route
 router.get('/login', (req,res) => {
     res.render('user/login');
 });
 
-//User register route
-router.get('/register', (req,res) => {
-    res.render('user/register');
-});
-
-//Login
+//Login User
 router.post('/login', (req,res,next) => {
     passport.authenticate('local', {
         successRedirect: '/memos',
@@ -25,79 +20,82 @@ router.post('/login', (req,res,next) => {
     })(req,res,next);
 });
 
-//Register User
-router.post('/register', (req,res) => {
-    let errors = [];
-
-    if(req.body.password != req.body.password2) {
-        errors.push({
-            text: 'passwords do not match'
-        });
-    }
-
-    if(req.body.password.length < 4) {
-        errors.push({
-            text: 'password must have at least 4 characters'
-        });
-    }
-
-    if(errors.length > 0) {
-        res.render('user/register', {
-            errors: errors,
-            username: req.body.username,
-            email: req.body.email
-        });
-    } else {
-        //check if user with email exists
-        User.findOne({email: req.body.email})
-            .then(user => {
-                if(user) {
-                    errors.push({
-                        text: 'user with this email already exists !'
-                    });
-                    res.render('user/login', {errors: errors});
-                } else {
-
-                    const newUser = new User ({
-                        username: req.body.username,
-                        email: req.body.email,
-                        password: req.body.password
-                    });
-
-                    console.log(`password before bcrypt ${newUser.password}`);
-
-                    bcrypt.genSalt(10, (err, salt) => {
-                        bcrypt.hash(newUser.password, salt, (err, hash) => {
-                            if (err) {
-                                errors.push({
-                                    text: err
-                                });
-                            }
-                            newUser.password = hash;
-                            newUser.save()
-                                .then(user => {
-                                    req.flash('success_msg', 'You re now registered, try to login');
-                                    res.redirect('/user/login');
-                                    return;
-                                }).catch(err => {
-                                errors.push({
-                                    text: err
-                                });
-                                res.render('user/register', {errors: errors});
-                                return;
-                            });
-                        })
-                    });
-                }
-            })
-    }
-});
-
 //Logout User
 router.get('/logout', (req,res) => {
-   req.logout();
-   req.flash('success_msg', 'You are logged out');
-   res.redirect('/');
+    req.logout();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/');
 });
+
+router.route('/register')
+    .get((req,res) => {
+    res.render('user/register');
+    })
+    .post((req,res) => {
+        let errors = [];
+
+        if(req.body.password != req.body.password2) {
+            errors.push({
+                text: 'passwords do not match'
+            });
+        }
+
+        if(req.body.password.length < 4) {
+            errors.push({
+                text: 'password must have at least 4 characters'
+            });
+        }
+
+        if(errors.length > 0) {
+            res.render('user/register', {
+                errors: errors,
+                username: req.body.username,
+                email: req.body.email
+            });
+        } else {
+            //check if user with email exists
+            User.findOne({email: req.body.email})
+                .then(user => {
+                    if(user) {
+                        errors.push({
+                            text: 'user with this email already exists !'
+                        });
+                        res.render('user/login', {errors: errors});
+                    } else {
+
+                        const newUser = new User ({
+                            username: req.body.username,
+                            email: req.body.email,
+                            password: req.body.password
+                        });
+
+                        console.log(`password before bcrypt ${newUser.password}`);
+
+                        bcrypt.genSalt(10, (err, salt) => {
+                            bcrypt.hash(newUser.password, salt, (err, hash) => {
+                                if (err) {
+                                    errors.push({
+                                        text: err
+                                    });
+                                }
+                                newUser.password = hash;
+                                newUser.save()
+                                    .then(user => {
+                                        req.flash('success_msg', 'You re now registered, try to login');
+                                        res.redirect('/user/login');
+                                        return;
+                                    }).catch(err => {
+                                    errors.push({
+                                        text: err
+                                    });
+                                    res.render('user/register', {errors: errors});
+                                    return;
+                                });
+                            })
+                        });
+                    }
+                })
+        }
+    });
 
 module.exports = router;
