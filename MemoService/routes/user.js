35db5642d72ad32ac7b3bf0be8f24a9e1e728643
adammendak <adam.mendak@gmail.com>
2018-03-12 11:43:03 +0100

    added

--- MemoService/routes/user.js
+++ MemoService/routes/user.js
@@ -3,13 +3,9 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.route('/login')
-    // .get(userController.get_login_form)
     .post(userController.log_user);
 
 router.route('/register')
-    // .get((req,res) => {
-    // res.render('user/register');
-    // })
     .post(userController.register_new_user);
 
 module.exports = router;
